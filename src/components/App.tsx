d72364321440389c2070168ce0156fcbda64fb36
Andrew Huth <git@huth.me>
2024-09-21 23:11:53 -0400

    Add some random dice values

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
-import {useDay, useActions} from '../store';
+import {useDay, useDice, useActions} from '../store';
 
 export default function App() {
   const day = useDay();
+  const dice = useDice();
   const actions = useActions();
 
   return (
     <div className="p-8 text-center">
-      <span>Day {day}</span>
+      <span>Day: {day}</span>
+      <br />
+      <span>Dice: {dice.join(', ')}</span>
+      <br />
       <button
         className="rounded-lg bg-blue-600 px-4 py-2 text-white shadow-md hover:bg-blue-700 active:bg-blue-800"
         onClick={actions.incrementDay}
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,9 @@
+import random from 'lodash/random';
 import {create} from 'zustand';
 
 type State = {
   day: number;
+  dice: Array<number | null>;
   actions: {
     incrementDay: () => void;
   };
@@ -10,11 +12,23 @@ type State = {
 const useStore = create<State>((set) => {
   return {
     day: 0,
+    dice: [],
     actions: {
-      incrementDay: () => set((state) => ({day: state.day + 1})),
+      incrementDay: () =>
+        set((state) => ({
+          day: state.day + 1,
+          dice: [
+            random(1, 6),
+            random(1, 6),
+            random(1, 6),
+            random(1, 6),
+            random(1, 6),
+          ],
+        })),
     },
   };
 });
 
 export const useDay = () => useStore((state) => state.day);
+export const useDice = () => useStore((state) => state.dice);
 export const useActions = () => useStore((state) => state.actions);
